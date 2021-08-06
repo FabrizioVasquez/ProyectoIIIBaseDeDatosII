0bf8175017ba12ec70f990ad4340261d8d411aed
jpmiraval <40766473+jpmiraval@users.noreply.github.com>
2021-08-05 23:55:26 -0500

    Update RTREE mateo

--- Backend/rtree.py
+++ Backend/rtree.py
@@ -1,8 +1,9 @@
 import face_recognition
-from os import listdir
+from os import listdir, read
 import numpy as np
 import pickle
 from os.path import isfile, join
+from numpy.lib.type_check import real
 from rtree import index
 
 
@@ -11,7 +12,7 @@ mypath = '/home/jpalexander1706/BDII/proyecto/ProyectoIIIBaseDeDatosII/Resources
 
 general_index = {}
 
-faces = []
+
 
 def read_faces(N):
     with open('/home/jpalexander1706/BDII/proyecto/ProyectoIIIBaseDeDatosII/Resources/dataset_faces.dat', 'rb') as f:
@@ -54,9 +55,25 @@ for i in range(5720):
     for j in range(64):
         arbolitos[j].insert(i, imagen[i][j])
 
+print(general_index)
+
+#query = read_faces(1)[0]
+name = "mateo.jpg"
+picture = face_recognition.load_image_file(name)
+query = (face_recognition.face_encodings(picture)[0])
+final_query = []
+counter_low = 0
+counter_top = 2
+count = 0
+for j in range(64):
+            final_query.append(query[counter_low : counter_top].tolist())
+            counter_top+=2
+            counter_low+=2
+
+
 Ans_generator = []
 for i in range(64):
-    Ans_generator.append(arbolitos[i].nearest([0.20, 0.35], 5))
+    Ans_generator.append(arbolitos[i].nearest(final_query[i], 5))
 
 real_answers = []
 for i in Ans_generator:
@@ -66,8 +83,9 @@ for i in Ans_generator:
     real_answers.append(next(i))
     real_answers.append(next(i))
 
+newlist = []
+frequency = []
+real_answers.sort()
 
-
-
-
-
+result = sorted(real_answers, key=real_answers.count, reverse = True)
+print(result)
